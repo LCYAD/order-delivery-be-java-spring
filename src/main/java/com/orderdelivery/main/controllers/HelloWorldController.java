da0287d3eace80e2a2ca29723d6f31a3a2283d47
Adrian Lee <alee.lcagency@gmail.com>
2024-05-11 15:18:43 +0800

    fix: java project launch issue (#2)

--- src/main/java/com/orderdelivery/main/controllers/HelloWorldController.java
+++ src/main/java/com/orderdelivery/main/controllers/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String helloWorld() {
         return "Hello World";
